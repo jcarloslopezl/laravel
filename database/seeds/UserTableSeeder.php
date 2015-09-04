7ad5963dc4038a4bb96a2a30d4ec73e7f4091747
Juan Carlos López <j.carloslopezl@hotmail.com>
2015-09-04 13:43:22 -0500

    Commit inicial

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Truncate users table
+        DB::table('users')->truncate();
+
+        factory(App\User::class)->create([
+            'name' => 'Juan Carlos',
+            'email' => 'jlopez@zegucom.com.mx',
+            'role' => 'admin',
+            'password' => 'test'
+        ]);
+        factory(App\User::class, 49)->create();
+
+    }
+}
